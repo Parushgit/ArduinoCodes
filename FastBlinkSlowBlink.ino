f62820e510458b21af4d96b454e008049e0b96c0
Parush Garg <parush.theultimate@gmail.com>
2018-04-09 17:16:58 -0400

    Add files via upload

---
+++ FastBlinkSlowBlink.ino
@@ -0,0 +1,33 @@
+/*
+  Blink
+
+  Turns an LED on for 0.5 second, then off for 2 second, repeatedly.
+  Fast and Slow blinking
+
+*/
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  int i = 0;
+  while(i < 5)
+  {
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    i++;
+  }
+  i = 0;
+  while(i < 5)
+  {
+    digitalWrite(13, HIGH);
+    delay(2000);
+    digitalWrite(13, LOW);
+    delay(2000);
+    i++;
+  }
+}
